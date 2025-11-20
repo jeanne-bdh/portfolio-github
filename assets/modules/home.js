9272776253f80af614c432144e9217f304f1bc87
eurajane <jbouchend@gmail.com>
2025-11-20 17:29:28 +0100

    add projects on home

--- assets/modules/home.js
+++ assets/modules/home.js
@@ -1,12 +1,19 @@
+import { Octokit } from "https://esm.sh/octokit";
+
 class Home {
     constructor() {
-        this.profilHTML = document.querySelector('.js-home-github-url')
-        this.descriptionHTML = document.querySelector('.js-home-description')
+        this.profilGithub = document.querySelector('.js-home-github-url')
+        this.descriptionGithub = document.querySelector('.js-home-description')
+
+        this.reposTitle = document.querySelectorAll('.js-home-repo-title')
+        this.reposDescription = document.querySelectorAll('.js-home-repo-description')
+        this.reposTags = document.querySelectorAll('.js-home-repo-tag')
         this.init()
     }
 
     init() {
         this.getGithubProfil()
+        this.getRepo()
     }
 
     getGithubProfil() {
@@ -20,9 +27,37 @@ class Home {
             })
     }
 
+    async getRepo() {
+        const octokit = new Octokit()
+        const response = await octokit
+            .request("GET /users/jeanne-bdh/repos")
+            .catch((error) => {
+                console.error("Erreur lors de l'appel API", error)
+            })
+
+        this.updateAllProjects(response.data)
+    }
+
     updateGithubProfil(APIdata) {
-        this.profilHTML.setAttribute("href", APIdata.html_url)
-        this.descriptionHTML.textContent = APIdata.bio
+        this.profilGithub.setAttribute("href", APIdata.html_url)
+        this.descriptionGithub.textContent = APIdata.bio
+    }
+
+    updateAllProjects(ProjetsData) {
+        let htmlIndex = 0
+
+        for (let i = 0; i < 3; i++) {
+            const repo = ProjetsData[i];
+
+            this.reposTitle[htmlIndex].textContent = repo.name;
+            this.reposDescription[htmlIndex].textContent = repo.description;
+
+            const titleProject = repo.name;
+            const descriptionProject = repo.description;
+            const languages = repo.topics;
+            
+            htmlIndex++
+        }
     }
 }
 
