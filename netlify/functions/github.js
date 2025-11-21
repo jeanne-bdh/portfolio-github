73b8f7f1064e978ffacfb98bb1009b007ddf1523
eurajane <jbouchend@gmail.com>
2025-11-21 14:30:16 +0100

    modify github.js

--- netlify/functions/github.js
+++ netlify/functions/github.js
@@ -4,16 +4,14 @@ import { Octokit } from "octokit";
 export async function handler(event, context) {
     try {
         const octokit = new Octokit({
-            auth: process.env.GITHUB_TOKEN, // Ton token est ici, côté serveur
+            auth: process.env.GITHUB_TOKEN,
         });
 
-        // Appel API GitHub
         const repos = await octokit.request("GET /users/jeanne-bdh/repos", {
             per_page: 100,
             sort: "updated",
         });
 
-        // Retourne les données au front
         return {
             statusCode: 200,
             body: JSON.stringify(repos.data),
